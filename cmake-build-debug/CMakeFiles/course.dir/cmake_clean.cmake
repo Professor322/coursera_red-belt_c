fb6b22de3a4e1479bab63831b61db404620f46c3
Nikolai <professor3222@gmail.com>
2020-04-08 01:40:13 +0300

    adding tasks on macroses

---
+++ cmake-build-debug/CMakeFiles/course.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/course.dir/week1/sort_by/sort_by.cpp.o"
+  "course"
+  "course.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
